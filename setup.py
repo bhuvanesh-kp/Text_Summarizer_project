1af0a7d19a81409f1294dd3d74b61cf36dbe5872
bhuvanesh-kp <bhuvaneshkpb@gmail.com>
2023-12-14 20:12:47 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ setuptools.setup(
     description="Text Summarizer",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker": "https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker": "https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
     },
     package_dir={"":"src"},
     packages=setuptools.find_packages(where="src")
