23800de4286f5449d52b4b976ea8dafb60aaa442
bhuvanesh-kp <bhuvaneshkpb@gmail.com>
2023-12-14 17:53:36 +0530

    1st

---
+++ src/textsummarizer/logging/__init__.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s:%(module)s: %(message)s]"
+log_dir = "logs"
+log_filepath = os.path.join(log_dir,exists_ok=True)
+os.makedirs(log_filepath,exist_ok=True)
+
+logging.basicConfig(level=logging.INFO,
+                    format=logging_str,
+                    handlers=[
+                        logging.StreamHandler(sys.stdout),
+                        logging.FileHandler(log_filepath)
+                    ]
+)
+
+logger = logging.getLogger("textsummarizerlogger")
