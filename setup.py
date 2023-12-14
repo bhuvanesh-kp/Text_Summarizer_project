1d5092d3bc141dc9634a2321071442b31c861ab7
bhuvanesh-kp <bhuvaneshkpb@gmail.com>
2023-12-14 17:34:11 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+__version__ = "0.0.0"
+REPO_NAME = "Text_Summarizer_project"
+AUTHOR_USER_NAME = "bhuvanesh-kp"
+SRC_REPO = "textsummarizer"
+AUTHOR_MAIL = "bhuvaneshkpb@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_MAIL,
+    description="Text Summarizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/",
+    project_urls={
+        "Bug Tracker": "https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src"),
+)
