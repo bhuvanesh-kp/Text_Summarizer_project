fb6fac34be71fe6948369386b73ef1912b30e087
bhuvanesh-kp <bhuvaneshkpb@gmail.com>
2023-12-14 18:01:49 +0530

    requirements added

--- src/textsummarizer/logging/__init__.py
+++ src/textsummarizer/logging/__init__.py
@@ -4,7 +4,7 @@ import logging
 
 logging_str = "[%(asctime)s: %(levelname)s:%(module)s: %(message)s]"
 log_dir = "logs"
-log_filepath = os.path.join(log_dir,exists_ok=True)
+log_filepath = os.path.join(log_dir,"running_logs.log")
 os.makedirs(log_filepath,exist_ok=True)
 
 logging.basicConfig(level=logging.INFO,
